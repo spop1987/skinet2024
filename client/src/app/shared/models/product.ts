aac87ca3dbe59b7edabc4ea9414dc1c2dc39cdb0
Sergio Ontiveros Perez <sergio.ontiveros.perez@outlook.com>
2024-11-05 21:08:13 -0500

    End of section 8

---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export type Product = {
+    id: number;
+    name: string;
+    description: string;
+    price: string;
+    pictureUrl: string;
+    type: string;
+    brand: string;
+    quantityInStock: string;
+}
